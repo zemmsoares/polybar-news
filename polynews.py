30c453848e0371e8d79ed6bf732b39c1d3f4636e
zemmsoares <zemmsoares@gmail.com>
2018-07-04 17:42:12 +0000

    Python script

--- polynews.py
+++ polynews.py
@@ -1,10 +1,18 @@
-import requests
-import json
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
-response = requests.get("https://your-heroku-server.herokuapp.com/news")
-data = response.text
-parsed = json.loads(data)
-provider = parsed["Provider"]
-article = parsed["Article"]
- 
-print(provider+' '+article)
+quote_page = 'https://www.dn.pt/ultimas.html'
+
+page = urlopen(quote_page)
+soup = BeautifulSoup(page, 'html.parser')
+
+name_box = soup.find('div', attrs={'class': 't-article-list-1-body'})
+name_box2 = name_box.find('li', attrs={'class': ''})
+
+title = name_box2.find('span');
+time = name_box2.find('time');
+linkz = name_box2.select("a[href*=html]")
+
+titleclean = title.text.strip()
+
+print(titleclean)
