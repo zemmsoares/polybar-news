80083f6c201f53ab2788f7aa44358b085a9b599f
zemmsoares <zemmsoares@gmail.com>
2019-10-19 03:26:09 +0100

    polynewsv2

---
+++ polynews.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import requests
+
+#get your api key at https://newsapi.org/
+
+api_key = "YOUR_API_KEY"
+
+#find sources & country codes at https://newsapi.org/sources
+
+sources = "ign,news24"
+country = ""
+
+try:
+    data = requests.get('https://newsapi.org/v2/top-headlines?apiKey='+api_key+'&sources='+sources+'&country='+country).json()
+
+    sourceName = data['articles'][0]['source']['name']
+    title = data['articles'][0]['title']
+    url = data['articles'][0]['url']
+
+    print(sourceName+': '+title)
+ 
+except requests.exceptions.RequestException as e:
+    print ('Something went wrong!')
+
+#API Powered by NewsAPI.org
