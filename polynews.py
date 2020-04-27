be3b5b0637e7859c56c5fc5abf06645101923270
zemmsoares <zemmsoares@gmail.com>
2020-04-27 18:18:21 +0100

    open news on browser

--- polynews.py
+++ polynews.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python
 
 import requests
+import os.path
+
+#path to polynews script
+
+save_path = '/home/zemmsoares/.config/polybar/polynews/'
 
 #get your api key at https://newsapi.org/
 
-api_key = "YOUR_API_KEY"
+api_key = "aad33b474b5b4d05b445332cf82d4b07"
 
 #find sources & country codes at https://newsapi.org/sources
 
-sources = "ign,news24"
-country = ""
+sources = ""
+country = "pt"
 
 try:
     data = requests.get('https://newsapi.org/v2/top-headlines?apiKey='+api_key+'&sources='+sources+'&country='+country).json()
@@ -19,6 +24,12 @@ try:
     url = data['articles'][0]['url']
 
     print(sourceName+': '+title)
+
+    path = os.path.join(save_path,"current_url.txt")         
+    f = open(path, "w")
+    f.write(url)
+    f.close()
+    
  
 except requests.exceptions.RequestException as e:
     print ('Something went wrong!')
