0aa4053f69f9e8e20712dca1dffa3d823e633cf7
zemmsoares <zemmsoares@gmail.com>
2023-07-04 23:58:09 +0100

    feat: :sparkles: Remove hardcoded news file path

--- get_news_status.sh
+++ get_news_status.sh
@@ -1,7 +1,7 @@
-#!/bin/bash  
-      
-echo "$(<$HOME/.config/polybar/scripts/news/current_news.txt)"
+#!/bin/bash
 
+# Directory of the script
+script_dir=$(dirname "$0")
 
-
-
+# Print the current news article
+echo "$(<"$script_dir/current_news.txt")"
