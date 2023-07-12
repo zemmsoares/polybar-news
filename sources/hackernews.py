9495191a91a6934430c232c7b72d2900fa90c741
zemmsoares <zemmsoares@gmail.com>
2023-07-12 14:07:53 +0100

    docs: :memo: Update documentation for i3blocks

--- sources/hackernews.py
+++ sources/hackernews.py
@@ -6,7 +6,6 @@ def get_news():
     response = requests.get('https://hacker-news.firebaseio.com/v0/topstories.json')
     story_ids = json.loads(response.text)[:number_news]
     stories = []
-    print(response)
 
     for id in story_ids:
         response = requests.get(f'https://hacker-news.firebaseio.com/v0/item/{id}.json')
@@ -15,6 +14,5 @@ def get_news():
         url = story.get('url', '')
         story_type = story.get('type', '')
         stories.append({'title': title, 'url': url, 'type': story_type})
-        print(id)
 
     return stories
